82bdc1af6b49dd61d587b7c16a49f9dcff439729
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-24 17:15:54 +0100

    Added id to success message in signup html. Implemented Signup test. Test failing as expected

--- src/test/java/com/cmcnally/udacity/project/cloudstorage/IntegrationTests/SignupTest.java
+++ src/test/java/com/cmcnally/udacity/project/cloudstorage/IntegrationTests/SignupTest.java
@@ -1,2 +1,62 @@
-package com.cmcnally.udacity.project.cloudstorage.IntegrationTests;public class SignupTest {
+package com.cmcnally.udacity.project.cloudstorage.IntegrationTests;
+
+import com.cmcnally.udacity.project.cloudstorage.PageObjects.SignupPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SignupTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    // Initialise web driver and page object
+    private static WebDriver driver;
+    private SignupPage signupPage;
+
+    // Initialise common test variables
+    private String testFirstName = "Joe";
+    private String testLastName = "Soap";
+    private String testUsername = "jsoap";
+    private String testPassword = "JoesPassw0rd";
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSignup() {
+        // Set up web driver and page objects for the test
+        driver.get("http://localhost:" + port +"/signup");
+        signupPage = new SignupPage(driver);
+
+        // Fill out the signup page text fields
+        signupPage.setFirstName(testFirstName);
+        signupPage.setLastName(testLastName);
+        signupPage.setUsername(testUsername);
+        signupPage.setPassword(testPassword);
+
+        // Click the submit button
+        signupPage.clickSubmit();
+
+        // Verify that the user is successfully created by checking the success message is displayed
+        assertTrue(driver.findElement(By.id("success-msg")).isDisplayed());
+    }
+
 }
