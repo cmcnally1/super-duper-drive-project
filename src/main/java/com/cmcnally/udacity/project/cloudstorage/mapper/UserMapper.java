9fed16e880a5fa181b759b83a454cdf1e844e4bf
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-24 16:41:11 +0100

    Implemented user mapper

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/UserMapper.java
@@ -1,4 +1,21 @@
 package com.cmcnally.udacity.project.cloudstorage.mapper;
 
+import com.cmcnally.udacity.project.cloudstorage.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface UserMapper {
+
+    // Select a user from the database
+    @Select("SELECT * FROM USERS WHERE username = #{username}")
+    User getUser(String username);
+
+    // Insert a new user into the database
+    @Insert("INSERT into USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstname}, #{lastname})")
+    @Options(useGeneratedKeys = true, keyProperty = "userid")
+    int insert(User user);
+
 }
