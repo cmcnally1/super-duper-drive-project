2fcfd2ff64d719969d15d72d92ad06abb03182ce
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-25 17:56:12 +0100

    Implemented login test. Failing as expected

--- src/test/java/com/cmcnally/udacity/project/cloudstorage/IntegrationTests/LoginTest.java
+++ src/test/java/com/cmcnally/udacity/project/cloudstorage/IntegrationTests/LoginTest.java
@@ -1,2 +1,85 @@
-package com.cmcnally.udacity.project.cloudstorage.IntegrationTests;public class LoginTest {
+package com.cmcnally.udacity.project.cloudstorage.IntegrationTests;
+
+import com.cmcnally.udacity.project.cloudstorage.PageObjects.LoginPage;
+import com.cmcnally.udacity.project.cloudstorage.PageObjects.SignupPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LoginTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    // Initialise web driver and page object
+    private static WebDriver driver;
+    private SignupPage signupPage;
+    private LoginPage loginPage;
+
+    // Initialise common test variables
+    private String testFirstName = "Joe";
+    private String testLastName = "Soap";
+    private String testUsername = "jsoap";
+    private String testPassword = "JoesPassw0rd";
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+    @Test
+    public void testLogin() {
+
+        /*
+            Preconditions:
+            The user must be signed up before they can log in.
+            Therefore, user being tested is signed up first.
+         */
+
+        // Set up web driver and page objects for the test
+        driver.get("http://localhost:" + port +"/signup");
+        signupPage = new SignupPage(driver);
+
+        // Fill out the signup page text fields
+        signupPage.setFirstName(testFirstName);
+        signupPage.setLastName(testLastName);
+        signupPage.setUsername(testUsername);
+        signupPage.setPassword(testPassword);
+
+        // Click the submit button
+        signupPage.clickSubmit();
+
+        /*
+            Test Login:
+            Once the user has signed up, test they can successfully login.
+         */
+
+        // Set up web driver and page objects for the test
+        driver.get("http://localhost:" + port +"/login");
+        loginPage = new LoginPage(driver);
+
+        // Fill out the login text fields
+        loginPage.setUsername(testUsername);
+        loginPage.setPassword(testPassword);
+
+        // Click the submit button
+        loginPage.clickSubmit();
+
+        // Verify that the home page is loaded indicating a successful login
+        assertEquals("Home", driver.getTitle());
+    }
 }
