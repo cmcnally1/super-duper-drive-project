3ba3d04c0ef770750d2cb66c6fbfaa53da81d412
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-25 18:56:28 +0100

    Update security configuration to user authentication service to check user logins

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/config/SecurityConfig.java
@@ -1,19 +1,33 @@
 package com.cmcnally.udacity.project.cloudstorage.config;
 
 
-/*
-    Security configuration class to define the authentication config and HTTP request security
- */
-
+import com.cmcnally.udacity.project.cloudstorage.services.AuthenticationService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*
+    Security configuration class to define the authentication config and HTTP request security
+ */
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private AuthenticationService authenticationService;
+
+    // Constructor
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    // Configure Spring to use Authentication Service to check logins
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(this.authenticationService);
+    }
 
     // Configure the security requirements for HTTP requests
     @Override
