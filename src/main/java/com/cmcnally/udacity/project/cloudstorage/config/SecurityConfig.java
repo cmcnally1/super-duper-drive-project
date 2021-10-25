232c07926c014108d90ef9e4022898301386ec2a
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-25 18:03:27 +0100

    Reenable Spring security and reimplement Security configuration

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/config/SecurityConfig.java
@@ -6,32 +6,33 @@ package com.cmcnally.udacity.project.cloudstorage.config;
  */
 
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig  {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-//    // Configure the security requirements for HTTP requests
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        // Permit all users from accessing the signup page and site resources
-//        // Permit only authenticated users from accessing all other pages
-//        http.authorizeRequests()
-//                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
-//                .anyRequest().authenticated();
-//
-//        // Set the login page and permit all users to access it
-//        http.formLogin()
-//                .loginPage("/login")
-//                .permitAll();
-//
-//        // Set the home page to be loaded on a successful login
-//        http.formLogin()
-//                .defaultSuccessUrl("/home", true);
-//
-//    }
+    // Configure the security requirements for HTTP requests
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // Permit all users from accessing the signup page and site resources
+        // Permit only authenticated users from accessing all other pages
+        http.authorizeRequests()
+                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated();
+
+        // Set the login page and permit all users to access it
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        // Set the home page to be loaded on a successful login
+        http.formLogin()
+                .defaultSuccessUrl("/home", true);
+
+    }
 }
