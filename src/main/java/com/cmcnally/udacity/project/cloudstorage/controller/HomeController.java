ce0951532c8e446f6d30a5ae1cd406baa1ac7691
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-25 18:57:11 +0100

    Implement GET mapping in Home controller to be able to load home page

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/controller/HomeController.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.cmcnally.udacity.project.cloudstorage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
 public class HomeController {
+
+    @GetMapping
+    public String getHomeView(){
+        return "home";
+    }
 }
