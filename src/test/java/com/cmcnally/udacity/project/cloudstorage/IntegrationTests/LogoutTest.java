e8f5fca3295ee29ff21aaeb2ae4ce69990da9f80
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-25 19:22:56 +0100

    Implemented logout test. Added an id to logged out message in login HTML

--- src/test/java/com/cmcnally/udacity/project/cloudstorage/IntegrationTests/LogoutTest.java
+++ src/test/java/com/cmcnally/udacity/project/cloudstorage/IntegrationTests/LogoutTest.java
@@ -1,2 +1,99 @@
-package com.cmcnally.udacity.project.cloudstorage.IntegrationTests;public class LogoutTest {
+package com.cmcnally.udacity.project.cloudstorage.IntegrationTests;
+
+import com.cmcnally.udacity.project.cloudstorage.PageObjects.HomePage;
+import com.cmcnally.udacity.project.cloudstorage.PageObjects.LoginPage;
+import com.cmcnally.udacity.project.cloudstorage.PageObjects.SignupPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LogoutTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    // Initialise web driver and page object
+    private static WebDriver driver;
+    private SignupPage signupPage;
+    private LoginPage loginPage;
+    private HomePage homePage;
+
+    // Initialise common test variables
+    private String testFirstName = "Jim";
+    private String testLastName = "Dim";
+    private String testUsername = "jdim";
+    private String testPassword = "JimsPassw0rd";
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+    @Test
+    public void testLogin() {
+
+        /*
+            Preconditions:
+            The user must be signed up and logged in before they can log out.
+            Therefore, user being tested is signed up and logged in first.
+         */
+
+        // Set up web driver and page objects for the test
+        driver.get("http://localhost:" + port +"/signup");
+        signupPage = new SignupPage(driver);
+
+        // Fill out the signup page text fields
+        signupPage.setFirstName(testFirstName);
+        signupPage.setLastName(testLastName);
+        signupPage.setUsername(testUsername);
+        signupPage.setPassword(testPassword);
+
+        // Click the submit button
+        signupPage.clickSubmit();
+
+        // Set up web driver and page objects for login
+        driver.get("http://localhost:" + port +"/login");
+        loginPage = new LoginPage(driver);
+
+        // Fill out the login text fields
+        loginPage.setUsername(testUsername);
+        loginPage.setPassword(testPassword);
+
+        // Click the submit button
+        loginPage.clickSubmit();
+
+        /*
+            Test Logout:
+            Click logout button and verify user logged out successfully
+         */
+
+        // Set up web driver and page objects for home page
+        driver.get("http://localhost:" + port +"/home");
+        homePage = new HomePage(driver);
+
+        // Click the logout button
+        homePage.clickLogout();
+
+        // Verify that the Login page is loaded and
+        // logged out message is displayed indicating a successful logout
+        assertEquals("Login", driver.getTitle());
+        assertTrue(driver.findElement(By.id("logout-msg")).isDisplayed());
+    }
 }
