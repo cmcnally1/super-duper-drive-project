b65633b8a888a456eb22df35d71c1eae2e132487
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-25 18:03:54 +0100

    Implemented login controller

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/controller/LoginController.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/controller/LoginController.java
@@ -1,4 +1,20 @@
 package com.cmcnally.udacity.project.cloudstorage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+    Login controller to get the login view and handle user input when logging in
+ */
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping
+    public String loginView() {
+        return "login";
+    }
+
 }
