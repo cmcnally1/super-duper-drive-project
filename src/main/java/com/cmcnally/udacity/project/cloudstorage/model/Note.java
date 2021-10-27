8bfacb77f70ba12c0621a2b26d447f19d2abcdec
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-27 19:06:09 +0100

    Implemented Note model class

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/model/Note.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/model/Note.java
@@ -1,4 +1,57 @@
 package com.cmcnally.udacity.project.cloudstorage.model;
 
+/*
+    Model class for Note
+ */
+
 public class Note {
+
+    // Instance variables for a note
+    private int noteid;
+    private String notetitle;
+    private String notedescription;
+    private int userid;
+
+    // Note constructor
+    public Note(String notetitle, String notedescription, int userid) {
+        this.notetitle = notetitle;
+        this.notedescription = notedescription;
+        this.userid = userid;
+    }
+
+    /*
+        Getters and Setters
+     */
+
+    public int getNoteid() {
+        return noteid;
+    }
+
+    public void setNoteid(int noteid) {
+        this.noteid = noteid;
+    }
+
+    public String getNotetitle() {
+        return notetitle;
+    }
+
+    public void setNotetitle(String notetitle) {
+        this.notetitle = notetitle;
+    }
+
+    public String getNotedescription() {
+        return notedescription;
+    }
+
+    public void setNotedescription(String notedescription) {
+        this.notedescription = notedescription;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
 }
