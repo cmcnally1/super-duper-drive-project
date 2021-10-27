b9a675cc900658d58ca1fcec78bf25d536ad8e88
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-27 18:05:14 +0100

    Implemented note adding elements in Home page object. Added various ids to elements in home HTML.

--- src/test/java/com/cmcnally/udacity/project/cloudstorage/PageObjects/HomePage.java
+++ src/test/java/com/cmcnally/udacity/project/cloudstorage/PageObjects/HomePage.java
@@ -7,14 +7,61 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
+    // Misc page elements
+
     @FindBy(id = "logoutButton")
     private WebElement logoutButton;
+
+    // Note tab page elements
+
+    @FindBy(id = "nav-notes")
+    private WebElement noteTab;
+
+    @FindBy(id = "new-note-button")
+    private WebElement newNoteButton;
+
+    @FindBy(id = "note-entry-rows")
+    private WebElement storedNoteRows;
+
+    @FindBy(id = "note-entry-title")
+    private WebElement storedNoteTitle;
+
+    @FindBy(id = "note-entry-desc")
+    private WebElement storedNoteDescription;
+
+    @FindBy(id = "note-title")
+    private WebElement newNoteTitleField;
+
+    @FindBy(id = "note-description")
+    private WebElement newNoteDescriptionField;
+
+    @FindBy(id = "noteSubmit")
+    private WebElement newNoteSubmitButton;
 
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
+    // Misc page methods
+
     public void clickLogout() {
         logoutButton.click();
     }
+
+    // Note methods
+
+    public void clickNoteTab() {
+        noteTab.click();
+    }
+
+    public void clickAddNote() {
+        newNoteButton.click();
+    }
+
+    public void addNewNote(String title, String description) {
+        newNoteTitleField.sendKeys(title);
+        newNoteDescriptionField.sendKeys(description);
+        newNoteSubmitButton.click();
+    }
+
 }
