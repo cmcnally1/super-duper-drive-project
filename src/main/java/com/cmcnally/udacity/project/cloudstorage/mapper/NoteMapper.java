67efda015fa38af4aab792038e50d98c458d7ced
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-27 19:12:16 +0100

    Implemented Note mapper

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/NoteMapper.java
@@ -1,4 +1,20 @@
 package com.cmcnally.udacity.project.cloudstorage.mapper;
 
+import com.cmcnally.udacity.project.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface NoteMapper {
+
+    // Select all notes for a user
+    @Select("SELECT * FROM NOTES WHERE userid = #{userid}")
+    Note getNote(int userid);
+
+    // Insert a new note into the database
+    @Insert("INSERT into NOTES (notetitle, notedescription, userid) VALUES(#{notetitle}, #{notedescription}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteid")
+    int insert(Note note);
 }
