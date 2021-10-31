c817664f62fdac08d3d43fbca7c43097562704e1
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-31 21:53:23 +0000

    Implemented File mapper with select and insert methods

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/FileMapper.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/FileMapper.java
@@ -1,4 +1,23 @@
 package com.cmcnally.udacity.project.cloudstorage.mapper;
 
+import com.cmcnally.udacity.project.cloudstorage.model.File;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
 public interface FileMapper {
+
+    // Select all files for a user
+    @Select("Select * FROM FILES WHERE userid = #{userid}")
+    List<File> getFiles(Integer userid);
+
+    // Insert a new file into the database
+    @Insert("INSERT into FILES (fileId, filename, contenttype, filesize, userid, filedata) VALUES(#{fileId}, #{filename}, #{contenttype}, #{filesize}, #{userid}, #{filedata})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileId")
+    int insert(File file);
+
 }
