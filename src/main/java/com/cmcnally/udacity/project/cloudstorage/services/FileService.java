474a2be5ec894e8093b3b996ecd227c0b00d6445
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-31 22:02:23 +0000

    Implemented file service with methods to add a file and get all files for logged in user

---
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/services/FileService.java
@@ -0,0 +1,35 @@
+package com.cmcnally.udacity.project.cloudstorage.services;
+
+import com.cmcnally.udacity.project.cloudstorage.mapper.FileMapper;
+import com.cmcnally.udacity.project.cloudstorage.model.File;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FileService {
+
+    private List<File> storedFiles;
+    private FileMapper fileMapper;
+    private AuthenticationService authenticationService;
+
+    // Constructor
+
+    public FileService(List<File> storedFiles, FileMapper fileMapper, AuthenticationService authenticationService) {
+        this.storedFiles = storedFiles;
+        this.fileMapper = fileMapper;
+        this.authenticationService = authenticationService;
+    }
+
+    // Method to add file to database via mapper
+    public void addFile(File file){
+        fileMapper.insert(file);
+    }
+
+    // Method to get the currently logged in user's files from database
+    public List<File> getStoredFiles() {
+        return new ArrayList<>(fileMapper.getFiles(authenticationService.getUserId()));
+    }
+
+}
