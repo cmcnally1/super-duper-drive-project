518fa743e38251a5e7c5a48decb4340b99b1a71e
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-11-03 19:35:56 +0000

    Implemented Credential model class. Implemented Credential mapper methods for adding credentials

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/CredentialMapper.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/mapper/CredentialMapper.java
@@ -1,9 +1,22 @@
 package com.cmcnally.udacity.project.cloudstorage.mapper;
 
+import com.cmcnally.udacity.project.cloudstorage.model.Credential;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface CredentialMapper {
 
+    // Select method for getting credentials stored for a user
+    @Select("Select * FROM CREDENTIALS WHERE userid = #{userid}")
+    List<Credential> getCredentials(Integer userid);
 
+    // Insert a new credential into the database
+    @Insert("INSERT into CREDENTIALS (url, username, key, password, userid) VALUES(#{url}, #{username}, #{key}, #{password}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialid")
+    int insert(Credential credential);
 }
