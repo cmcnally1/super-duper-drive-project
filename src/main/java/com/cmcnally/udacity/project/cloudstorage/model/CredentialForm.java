dd07b6ae80323e6465a3bc0b548b6b9debb574ba
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-11-04 18:06:09 +0000

    Implemented model class for the credential form

--- src/main/java/com/cmcnally/udacity/project/cloudstorage/model/CredentialForm.java
+++ src/main/java/com/cmcnally/udacity/project/cloudstorage/model/CredentialForm.java
@@ -1,2 +1,46 @@
-package com.cmcnally.udacity.project.cloudstorage.model;public class CredentialForm {
+package com.cmcnally.udacity.project.cloudstorage.model;
+
+/*
+    Model class for credential creation form
+ */
+public class CredentialForm {
+
+    private Integer formCredentialId;
+    private String formCredentialUrl;
+    private String formCredentialUsername;
+    private String formCredentialPassword;
+
+
+    // Getters and Setters
+    public Integer getFormCredentialId() {
+        return formCredentialId;
+    }
+
+    public void setFormCredentialId(Integer formCredentialId) {
+        this.formCredentialId = formCredentialId;
+    }
+
+    public String getFormCredentialUrl() {
+        return formCredentialUrl;
+    }
+
+    public void setFormCredentialUrl(String formCredentialUrl) {
+        this.formCredentialUrl = formCredentialUrl;
+    }
+
+    public String getFormCredentialUsername() {
+        return formCredentialUsername;
+    }
+
+    public void setFormCredentialUsername(String formCredentialUsername) {
+        this.formCredentialUsername = formCredentialUsername;
+    }
+
+    public String getFormCredentialPassword() {
+        return formCredentialPassword;
+    }
+
+    public void setFormCredentialPassword(String formCredentialPassword) {
+        this.formCredentialPassword = formCredentialPassword;
+    }
 }
